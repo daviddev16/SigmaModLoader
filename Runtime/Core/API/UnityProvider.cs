054e41b4e103e939179636ef3579b63c42cbcc80
daviddev16 <davidduartepinheiro@gmail.com>
2020-12-23 15:55:30 -0300

    Added UnityProvider for logging.

---
+++ Runtime/Core/API/UnityProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sigma.Logging
+{
+    public class UnityProvider : IProvider
+    {
+        public void LogBase(string message, Level level, Type logType)
+        {
+            if(level == Level.INFO)
+            {
+                Debug.Log(message);
+            }
+            else if(level == Level.WARNING)
+            {
+                Debug.LogWarning(message);
+            }
+            else if(level == Level.ERROR || level == Level.CRITICAL)
+            {
+                Debug.LogError(message);
+            }
+            else
+            {
+                Debug.LogWarning(message);
+            }
+        }
+
+        public void LogBase(string message)
+        {
+            Debug.Log(message);
+        }
+
+        public void LogBlankLine()
+        {
+            Debug.Log("");
+        }
+    }
+}
